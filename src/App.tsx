eff3f84c0389ab253b8ce53dff21f1b52ddadb06
Babak Ashrafi <52350580+babak271@users.noreply.github.com>
2025-05-16 19:00:55 +0200

    make image and video bigger

--- src/App.tsx
+++ src/App.tsx
@@ -34,12 +34,12 @@ function App() {
 
     const listItem = day.map(item =>
       <div className="flex justify-between border-2 border-solid border-sky-100 rounded-sm my-1 p-3">
-        <div className="flex flex-col w-1/2">
+        <div className="flex flex-col">
           <h2 className="text-lg text-center">{item.name}</h2>
-          <p className="text-sm">{item.repeat}</p>
-            <iframe className="h-20 w-20" src={item.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+          <p className="text-sm mb-3">{item.repeat}</p>
+          <iframe className="h-40 w-40" src={item.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
-        <img className="h-40 w-40" src={item.image} />
+        <img className="h-50 w-50" src={item.image} />
       </div>
     );
 
