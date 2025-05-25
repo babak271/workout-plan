b9380c9befb3567d390a9d0ead25a20c837c6190
Babak Ashrafi <52350580+babak271@users.noreply.github.com>
2025-05-25 18:10:14 +0200

    Revert "try to fix the footer issue on mobile"
    This reverts commit ef2f9b51cdbfff79e361268c5498542b2ac2cdf3.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,19 +3,16 @@ import EditIcon from '../assets/edit.svg?react'
 
 export default function footer() { 
   return (
-    <>
-      <div className="absolute bottom-0 left-0 w-full -pb-50 sticky"></div>
-      <div className="realtive bg-white flex justify-between items-center py-3 bg-white shadow-lg rounded sticky bottom-0 left-0 w-full">
-        <div className="flex flex-col mx-8 items-center">
-          <HomeIcon className="w-6 fill-(--color-primary)" />
-          <p className="text-xs">Home</p>
-        </div>
-        <div className="flex flex-col mx-8 items-center">
-          <EditIcon className="w-6 fill-(--color-secondary)" />
-          <p className="text-xs">Edit</p>
-        </div>
+    <div className="flex justify-between items-center py-3 bg-white shadow-lg rounded sticky bottom-0 left-0 w-full">
+      <div className="flex flex-col mx-8 items-center">
+        <HomeIcon className="w-6 fill-(--color-primary)" />
+        <p className="text-xs">Home</p>
       </div>
-    </>
+      <div className="flex flex-col mx-8 items-center">
+        <EditIcon className="w-6 fill-(--color-secondary)" />
+        <p className="text-xs">Edit</p>
+      </div>
+    </div>
   )
 }
 
