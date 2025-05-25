133cabfae93466a9e5da9182c238dcffd10401fe
Babak Ashrafi <52350580+babak271@users.noreply.github.com>
2025-05-25 18:52:22 +0200

    fix footer bleeding on mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,16 +3,19 @@ import EditIcon from '../assets/edit.svg?react'
 
 export default function footer() { 
   return (
-    <div className="flex justify-between items-center py-3 bg-white shadow-lg rounded fixed bottom-0 left-0 w-full h-16">
-      <div className="flex flex-col mx-8 items-center">
-        <HomeIcon className="w-6 fill-(--color-primary)" />
-        <p className="text-xs">Home</p>
-      </div>
-      <div className="flex flex-col mx-8 items-center">
-        <EditIcon className="w-6 fill-(--color-secondary)" />
-        <p className="text-xs">Edit</p>
-      </div>
-    </div>
+    <>
+      <div className="sticky -bottom-20 w-full pb-30 z-40 bg-white pointer-events-none"></div>
+      <footer className="flex justify-between items-center z-50 py-3 bg-white shadow-lg rounded fixed bottom-0 left-0 w-full h-16">
+        <div className="flex flex-col mx-8 items-center">
+          <HomeIcon className="w-6 fill-(--color-primary)" />
+          <p className="text-xs">Home</p>
+        </div>
+        <div className="flex flex-col mx-8 items-center">
+          <EditIcon className="w-6 fill-(--color-secondary)" />
+          <p className="text-xs">Edit</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,8 @@
 export default function header() { 
   return (
-    <>
-      <div className="flex justify-center py-4 border-b border-slate-200">
-        <p className="text-lg">Workout Plan</p>
-      </div>
-    </>
+    <header className="flex justify-center py-4 border-b border-slate-200">
+      <p className="text-lg">Workout Plan</p>
+    </header>
   )
 }
 
