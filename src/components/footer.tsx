3ff8cbf23c2f62f16300a77e10ac486752ba2262
Babak Ashrafi <52350580+babak271@users.noreply.github.com>
2025-05-25 18:28:30 +0200

    try to fix the footer issue on mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import EditIcon from '../assets/edit.svg?react'
 
 export default function footer() { 
   return (
-    <div className="flex justify-between items-center py-3 bg-white shadow-lg rounded fixed bottom-0 left-0 w-full">
+    <div className="flex justify-between items-center py-3 bg-white shadow-lg rounded fixed bottom-0 left-0 w-full h-16">
       <div className="flex flex-col mx-8 items-center">
         <HomeIcon className="w-6 fill-(--color-primary)" />
         <p className="text-xs">Home</p>
